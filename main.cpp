35217b8bc38c096a883cc1874b16ba622a5c9dc7
Neil <ebox381@gmail.com>
2025-12-23 01:50:35 -0500

    calcure

--- main.cpp
+++ main.cpp
@@ -5,19 +5,10 @@
 // clang++ -std=c++17 main.cpp -o main && ./main:     and compile with these flags
 
 // I just did simply:     alias g++='clang++ -std=c++17'
+//open calcure as well when starting
 
 int main() {
-    long standard = __cplusplus;
-
-    // Map the macro value to the C++ standard name
-    if (standard == 199711L) std::cout << "C++98/C++03" << std::endl;
-    else if (standard == 201103L) std::cout << "C++11" << std::endl;
-    else if (standard == 201402L) std::cout << "C++14" << std::endl;
-    else if (standard == 201703L) std::cout << "C++17" << std::endl;
-    else if (standard == 202002L) std::cout << "C++20" << std::endl;
-    else if (standard > 202002L) std::cout << "C++23 or later (code: " << standard << ")" << std::endl;
-    else std::cout << "Pre-C++98 or unknown version (code: " << standard << ")" << std::endl;
-
+    std::cout << "HELLO WORLD" << std::endl;
     return 0;
 }
 
